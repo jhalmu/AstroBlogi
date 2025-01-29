769072afdc96e5c3bbe2be42487993b7b3b730bb
Juha Halmu <juha.halmu@gmail.com>
2025-01-29 14:26:40 +0200

    fix:autorestart

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,10 +1,15 @@
-// ecosystem.config.cjs
 module.exports = {
-    apps: [{
-        name: "astro-watcher",
-        script: "./build-watcher.js",
-        env: {
-            NODE_ENV: "production"
-        }
-    }]
+  apps: [
+    {
+      name: "astro-build",
+      script: "pm2 flush && npm",
+      args: "run build",
+      watch: ["src/content/blog"],
+      ignore_watch: ["node_modules", ".astro", "dist"],
+      autorestart: false,  // Estää automaattisen uudelleenkäynnistyksen
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
 }
